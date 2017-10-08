507e160ee5c5e035bec1e715092178186ca92fc8
Autioch <autioch@gmail.com>
2017-10-08 09:52:25 +0200

    Added server generator.

---
+++ generators/server/index.js
@@ -0,0 +1,53 @@
+const Generator = require('yeoman-generator');
+const readdir = require('fs-readdir-recursive');
+
+module.exports = class extends Generator {
+  prompting() {
+    return this
+      .prompt([{
+        type: 'input',
+        name: 'accountName',
+        message: 'Your account name',
+        'default': this.user.git.name()
+      }, {
+        type: 'input',
+        name: 'projectName',
+        message: 'Your project name',
+        'default': 'Example Module'
+      }, {
+        type: 'input',
+        name: 'packageName',
+        message: 'Your package name',
+        'default': 'example-module'
+      }, {
+        type: 'input',
+        name: 'codeName',
+        message: 'Name in the code',
+        'default': 'exampleModule'
+      }])
+      .then((answers) => {
+        this.props = answers;
+      });
+  }
+
+  writing() {
+    const filesToCopy = readdir(this.sourceRoot(), () => true);
+    const templateData = this.props;
+
+    filesToCopy.forEach((fileName) => {
+      this.fs.copyTpl(
+        this.templatePath(fileName),
+        this.destinationPath(fileName.replace(/^_/, '')),
+        templateData
+      );
+    });
+  }
+
+  install() {
+    this.log('npm install & npm install --only=dev');
+    this.runInstall('npm');
+    this.runInstall('npm', '', {
+      only: 'dev'
+    });
+  }
+};
---
+++ generators/server/templates/readme.md
@@ -0,0 +1,17 @@
+# <%= packageName %>
+
+*Work in progress.*
+
+
+## Installation
+`npm i <%= packageName %>`
+
+## Usage
+
+```javascript
+
+const <%= codeName %> = require('<%= packageName %>');
+
+import  <%= codeName %>  from '<%= packageName %>';
+
+```
